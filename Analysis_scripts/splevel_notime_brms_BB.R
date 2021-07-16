8a0e52f127aca69457db5b06323b715d6eed8003
Lisa Rosenthal <lrosenthal@ucdavis.edu>
2021-07-16 07:20:17 -0700

    ran models of species-level prev with all trays. prior for phi ~ exponenital(1). had slight effecst in which models performed best but varialbe estitmatets similar

--- Analysis_scripts/splevel_notime_brms_BB.R
+++ Analysis_scripts/splevel_notime_brms_BB.R
@@ -1,5 +1,4 @@
-# understanding individual-level disease risk in the four different treatments
-# models run seperately for each treatment
+# understanding species-level disease risk using data from all trays
 
 
 rm(list = ls())
@@ -13,7 +12,7 @@ sumtozero <- function(df){
   contrasts(df$day) <- contr.sum( length(unique( df$day )))
   return(df)
 }
-source('Stan_models/betabinom_custom_brms.R') #for using beta-binomial likelihood with brms
+source('Analysis_scripts/betabinom_custom_brms.R') #for using beta-binomial likelihood with brms
 
 #ggplot theme stuff
 theme_set(theme_classic())
@@ -22,13 +21,10 @@ noaxislab <- theme(axis.title = element_blank(), plot.title = element_text(hjust
 #===============================================================================
 #load data
 #===============================================================================
-#M0: zscored(sqrt(radish density))
-#M1: zscored(density of others) + zscored(sqrt(radish density))
-#M2: zscored(density of others) + zscored(sqrt(radish density)) + zscored(sqrt(arugula density))*radish
 
-plantdf <- read_csv('output/plant_level_data.csv')
-traydf <- read_csv('output/tray_level_data.csv')
-trts <- readRDS('output/treatments_list.RDS')
+plantdf <- read_csv('Data/plant_level_data.csv')
+traydf <- read_csv('Data/tray_level_data.csv')
+
 
 #merge data sets
 plantdata <- plantdf %>% 
@@ -43,8 +39,7 @@ traydata <- traydf %>%
   filter(trayID < 300) %>% #remove uninoculated trays
   filter(trayID != 132) %>%  #and remove the unwatered tray
   mutate(n_others = n_allP.na - n_radish, 
-         n_others2 = n_allP.na - n_radish - n_arugula, 
-         radish_10 = as.numeric(n_radish >= 10)) %>% 
+         n_others2 = n_allP.na - n_radish - n_arugula) %>% 
   mutate_at(vars(temp_PM, richness, n_others, n_others2, n_allP.na), list(z=zscore)) %>% 
   mutate_at(vars(n_radish:n_butter), list(zsq = function(x) zscore(sqrt(x)))) %>% 
   mutate_at(vars(n_radish:n_butter), list(p = function(x) x/.$n_allP.na))
@@ -66,8 +61,7 @@ merged <- sumtozero(merged)
 # 3:      nothers, nradish
 # 4:      nothers2, nradish, narugula
 # 5:      nradish, narugula...nbutter
-# 6:      nradish*(narugula...nbutter)
-# 7:      nradish, narugula
+# 6:      nradish, narugula
 
 bf0 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + (1|trayID) + (1 + richness_z | spID))
 bf1 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + n_allP.na_z + (1|trayID) + (1 + richness_z | spID))
@@ -75,16 +69,16 @@ bf2 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + n_radish_zsq + (1
 bf3 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + n_others_z + n_radish_zsq + (1|trayID) + (1 + richness_z | spID))
 bf4 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + n_others2_z + n_radish_zsq + n_arugula_zsq + (1|trayID) + (1 + richness_z | spID))
 bf5 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + n_radish_zsq + n_arugula_zsq + n_basil_zsq + n_red_rom_zsq + n_green_rom_zsq + n_butter_zsq + (1|trayID) + (1 + richness_z | spID))
-bf6 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + n_radish_zsq + n_arugula_zsq + n_basil_zsq + n_red_rom_zsq + n_green_rom_zsq + n_butter_zsq + n_green_rom_zsq:radish_10 + n_butter_zsq:radish_10 + (1|trayID) + (1 + richness_z | spID))
-bf7 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + n_radish_zsq + n_arugula_zsq + (1|trayID) + (1 + richness_z | spID))
+bf6 <- bf(formula = I|vint(n) ~ day + temp_PM_z + richness_z + n_radish_zsq + n_arugula_zsq + (1|trayID) + (1 + richness_z | spID))
 
 fit0 <- brm(bf0, data = merged,
             prior = c(prior(normal(0, 1.5), class = Intercept),
                       prior(normal(0, 1), class = b),
                       prior(lkj(2), class = cor),
-                      prior(normal(0,1), class = sd)),
+                      prior(normal(0,2), class = sd), 
+                      prior(exponential(1), class = phi)),
             family = beta_binomial2, stanvars = stanvars,
-            iter = 2000, chains = 1, cores = 1, 
+            iter = 2000, chains = 4, cores = 4, 
             control = list(adapt_delta = 0.95, max_treedepth = 15), 
             save_pars = save_pars(all = TRUE))
 fit1 <- update(fit0, formula. = bf1, newdata = merged, save_pars = save_pars(all = TRUE))
@@ -93,8 +87,6 @@ fit3 <- update(fit0, formula. = bf3, newdata = merged, save_pars = save_pars(all
 fit4 <- update(fit0, formula. = bf4, newdata = merged, save_pars = save_pars(all = TRUE))
 fit5 <- update(fit0, formula. = bf5, newdata = merged, save_pars = save_pars(all = TRUE))
 fit6 <- update(fit0, formula. = bf6, newdata = merged, save_pars = save_pars(all = TRUE))
-fit7 <- update(fit0, formula. = bf7, newdata = merged, save_pars = save_pars(all = TRUE),
-               iter = 2000, chains = 4, cores = 4)
 
 
 
@@ -104,12 +96,7 @@ fit7 <- update(fit0, formula. = bf7, newdata = merged, save_pars = save_pars(all
 #stan functions need to be exposed for each fit. produces lots of warnings, supress them.
 expose_functions(fit0, vectorize = T)
 
-pp_check(fit7, nsamples = 50)
-
-# a0 + 
-fit5 %>% 
-  spread_draws(b_Intercept) %>% 
-  median_hdci()
+pp_check(fit3, nsamples = 50)
 
 K0 <- kfold(fit0, K = 10)
 K1 <- kfold(fit1, K = 10)
@@ -118,32 +105,34 @@ K3 <- kfold(fit3, K = 10)
 K4 <- kfold(fit4, K = 10)
 K5 <- kfold(fit5, K = 10)
 K6 <- kfold(fit6, K = 10)
-K7 <- kfold(fit7, K = 10)
-loo_compare(K0, K1, K2, K3, K4, K5, K6, K7)
-loo_compare(K0, K1, K2, K3, K4, K5, K7)
+loo_compare(K0, K1, K2, K3, K4, K5, K6)
 # elpd_diff se_diff
-# fit7   0.0       0.0  
-# fit3  -5.7       5.5  
-# fit5  -9.1       5.0  
-# fit2 -15.7       7.3  
-# fit4 -16.4      10.1  
-# fit0 -32.9       9.4  
-# fit1 -57.9      10.1  
+# fit2   0.0       0.0  #rad only
+# fit4  -5.8       4.4  #rad, arug, others
+# fit5 -10.1       9.0  #all ind spp
+# fit6 -14.9       7.3  #rad, arug
+# fit3 -15.0       7.7  #rad, ohters
+# fit0 -39.4       9.7  
+# fit1 -46.7       9.4 
 loo_compare(K0, K1, K2, K3, K4, K5)
 # elpd_diff se_diff
-# fit3   0.0       0.0  
-# fit5  -3.4       5.9  
-# fit2 -10.0       7.1  
-# fit4 -10.7      10.4  
-# fit0 -27.2      10.1  
-# fit1 -52.2      10.6 
+# fit2   0.0       0.0  
+# fit4  -5.8       4.4  
+# fit5 -10.1       9.0  
+# fit3 -15.0       7.7  
+# fit0 -39.4       9.7  
+# fit1 -46.7       9.4  
 loo_compare(K0, K1, K3, K4, K5)
 # elpd_diff se_diff
-# fit3   0.0       0.0  
-# fit5  -3.4       5.9  
-# fit4 -10.7      10.4  
-# fit0 -27.2      10.1  
-# fit1 -52.2      10.6  
+# fit4   0.0       0.0  #rad, arug, others
+# fit5  -4.3       8.5  #all ind spp
+# fit3  -9.2       7.7  #rad, others
+# fit0 -33.6       9.2  #rich only
+# fit1 -40.9       8.6  #dens
+write_rds(fit3, 'Outputs/splevel_alltrays_fit3.rds')
+write_rds(fit4, 'Outputs/splevel_alltrays_fit4.rds')
+write_rds(fit5, 'Outputs/splevel_alltrays_fit5.rds')
+
 #===============================================================================
 #posterior predictions, plotting for only 1 species
 #===============================================================================
@@ -172,13 +161,13 @@ predictions <- matrix(NA, ncol = nrow(newdat), nrow = nrow(B))
 for(i in 1:nrow(B)){
   predictions[i,] <- t(as.matrix(newdat) %*% B[i,])
 }
-predictions <- inv_logit(predictions)
+predictions <- inv_logit_scaled(predictions)
 
 #summarize predictions
 predictionsdf <- cbind.data.frame(newdat, 
                  data.frame(median = apply(predictions, 2, median), 
-                            lower = apply(predictions, 2, HPDI)[1,],
-                            upper = apply(predictions, 2, HPDI)[2,] ))
+                            lower = apply(predictions, 2, rethinking::HPDI)[1,],
+                            upper = apply(predictions, 2, rethinking::HPDI)[2,] ))
 
 
 #plot predictions against observations. Do for a single species.
