c2a83010113d9b0996a9e64ea423878ee9b2f569
Lisa Rosenthal <lrosenthal@ucdavis.edu>
2021-07-18 08:00:36 -0700

    adding script to make figures showing richness vs radish (either proporiton or # radish)

--- Analysis_scripts/prop_radish_supplementary.R
+++ Analysis_scripts/prop_radish_supplementary.R
@@ -1,13 +1,32 @@
 df <- read_csv('data/plant_level_data.csv')
 head(df)
 
-df %>% 
+tmp <- df %>% 
   filter(tray_type %in% c('regular', 'augmented')) %>% 
   group_by(trayID, tray_type, richness) %>% 
-  summarise(rad_prev = sum(spID == 1) / n()) %>% 
-  ggplot(., aes(richness, rad_prev, group = tray_type)) +
+  summarise(rad_prev = sum(spID == 1) / n(), 
+            rad_dens = sum(spID == 1)) 
+
+#overall correlation??
+cor(tmp$richness, tmp$rad_prev) # -0.3255051
+cor(tmp$richness, tmp$rad_dens) # -0.09505544
+
+tmp2 <- tmp %>% filter(tray_type == 'regular')
+#barely made a dent, but whatever.
+cor(tmp2$richness, tmp2$rad_prev) #-0.3594578. 
+cor(tmp2$richness, tmp2$rad_dens) # -0.1589976
+
+
+
+ggplot(tmp, aes(richness, rad_prev, group = tray_type)) +
   geom_jitter(height= 0,width = .1, alpha = .7, aes(color = tray_type)) +
   labs(x = 'Richness', y = 'Proportion radish', color = 'Tray type') +
   theme(legend.position = 'bottom')
 
 ggsave('Figures/prop_rad_supplementary.pdf', width = 4, height = 3)
+
+
+ggplot(tmp, aes(richness, rad_dens, group = tray_type)) +
+  geom_jitter(height= 0,width = .1, alpha = .7, aes(color = tray_type)) +
+  labs(x = 'Richness', y = 'Proportion radish', color = 'Tray type') +
+  theme(legend.position = 'bottom')
